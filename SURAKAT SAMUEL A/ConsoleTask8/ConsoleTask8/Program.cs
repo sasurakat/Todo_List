61560e9e34992a009c30128a16cd76ab54037df4
Surakat Samuel <Surakat.s@bullseye.local>
2023-05-12 08:37:21 +0100

    Todo Console App

---
+++ SURAKAT SAMUEL A/ConsoleTask8/ConsoleTask8/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ConsoleTask8
+{
+    public static class Program
+    {
+        public static readonly string startMessage = "================================================ TODO APPLICATION  ===================================================";
+        public static readonly string greetingMessage = "\"Welcome to Task Manager! ";
+        public static readonly string userAuthenticationMessage = "1. Register  \n2. Login \n0. Exist ";
+        public static readonly string userProfileMessage = "\n 1.Add Task\n 2.View Task \n 3.Edit Task \n 4.Delete Task \n 5.Complete Task \n 6.Logout ";
+        public static User currentUser;
+
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>();
+
+
+            while (true)
+            {
+                Console.WriteLine(startMessage);
+                Console.WriteLine(greetingMessage);
+                Console.WriteLine(userAuthenticationMessage);
+
+                string option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        RegisterUI.Register(users);
+                        break;
+                    case "2":
+                        LoginUI.Login(users, ref currentUser);
+                        break;
+                    case "0":
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine("Goodbye!");
+                        return;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Invalid input. Enter between {1,2,0}.");
+                        break;
+                }
+                Console.WriteLine();
+
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
+
+            }
+
+        }
+
+    }
+}
---
+++ SURAKAT SAMUEL A/ConsoleTask8/ConsoleTask8/TodoDetail.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTask8
+{
+    public class TodoDetail
+    {
+        public int Register { get; set; }
+        public string Login { get; set; }
+        public string Exist { get; set; }
+
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+       
+       
+
+
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+    }
+
+    public class Task
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public int ItemId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Duedate { get; set; }
+        public PeriorityLevel PeriorityLevel { get; set; }
+        public int UserID { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public Task( int itemId, string title, string description, string duedate, PeriorityLevel periorityLevel, int userId)
+        {
+            Title = title;
+            Description = description;
+            Duedate = duedate;
+            PeriorityLevel = periorityLevel;
+            UserID = userId;
+            IsCompleted = false;
+            Id = new Guid();
+            ItemId = itemId;
+        }
+    }
+    public enum PeriorityLevel
+    {
+        low = 1, medium =2, high =3
+    }
+   
+    public class CurrentUser
+    {
+        public static int Id { get; set; }
+        public Guid PersonId { get; }= Guid.NewGuid();
+    }
+
+    
+
+
+}
